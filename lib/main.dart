bea895c0decd854510e9dfd639c57d32e6a41bac
pduebel <pnduebel@gmail.com>
2021-10-02 11:51:39 +0100

    Change question on button press, separate question text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 
+import './questions.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Personality Quiz')),
         body: Column(
-          children: const [
-            Text('The question!'),
+          children: [
+            Question(questions[_questionIndex]),
             ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: null,
+              child: const Text('Answer 1'),
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: null,
+              child: const Text('Answer 2'),
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: null,
+              child: const Text('Answer 3'),
+              onPressed: _answerQuestion,
             ),
           ],
         ),
