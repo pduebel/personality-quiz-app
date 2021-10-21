d59607d6cf5f2da9bba97e61a5bdc3d5a3c7026b
pduebel <pnduebel@gmail.com>
2021-10-21 07:29:37 +0100

    add restart button to result screen

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -19,16 +20,15 @@ class Result extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text(
-            resultPhrase,
-            style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
+    return Center(
+      child: Column(children: <Widget>[
+        Text(
+          resultPhrase,
+          style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
         ),
-      ],
+        TextButton(child: Text('Restart Quiz!'), onPressed: this.resetHandler),
+      ]),
     );
   }
 }
